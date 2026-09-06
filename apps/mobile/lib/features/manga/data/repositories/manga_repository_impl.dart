20ff594dc8e7e5ac772048fa9f438958347c92ac
FrezzDEV <102240197+FrezzDEV@users.noreply.github.com>
2026-09-06 18:53:39 +0300

    fix(mobile): correct manga repository imports

--- apps/mobile/lib/features/manga/data/repositories/manga_repository_impl.dart
+++ apps/mobile/lib/features/manga/data/repositories/manga_repository_impl.dart
@@ -1,4 +1,4 @@
-import '../../../core/network/api_client.dart';
+import '../../../../core/network/api_client.dart';
 import '../../domain/entities/manga.dart';
 import '../../domain/repositories/manga_repository.dart';
 
